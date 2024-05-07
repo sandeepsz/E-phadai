8b6a322bd2ce10975fc2636bdefeffe726bb4f63
sandip <sandeep47nepali@gmail.com>
2024-05-07 13:34:55 +0545

    show warning banner for unpublished chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -9,6 +9,7 @@ import ChapterTitle from "./_components/chapter-title";
 import ChapterDescription from "./_components/chapter-description";
 import AccessForm from "./_components/access-form";
 import ChapterVideoForm from "./_components/chapter-video";
+import Banner from "@/components/banner";
 
 const ChapterEditPage = async ({
   params,
@@ -45,77 +46,87 @@ const ChapterEditPage = async ({
   const completeSteps = `${completeFields}/${totalFields}`;
 
   return (
-    <div className="p-6">
-      <div className="flex items-center justify-between">
-        <div className="w-full">
-          <Link
-            href={`/teacher/courses/${params.courseId}`}
-            className="flex items-center gap-2  text-sm hover:opacity-80 transition-all mb-6"
-          >
-            <ArrowLeft className="w-4 h-4" />
-            Back to course
-          </Link>
-          <div className="flex items-center justify-between w-full">
-            <div className="flex flex-col gap-y-2">
-              <h1 className="text-2xl font-medium">Chapter setup</h1>
-              <span className="text-sm text-slate-600">
-                Complete all fields {completeSteps}
-              </span>
+    <>
+      {!chapter.isPublished && (
+        <Banner
+          variant="warning"
+          label="This chapter is not publish so this is Not visible."
+        />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <Link
+              href={`/teacher/courses/${params.courseId}`}
+              className="flex items-center gap-2  text-sm hover:opacity-80 transition-all mb-6"
+            >
+              <ArrowLeft className="w-4 h-4" />
+              Back to course
+            </Link>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">Chapter setup</h1>
+                <span className="text-sm text-slate-600">
+                  Complete all fields {completeSteps}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10">
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center gap-x-2 ">
+                <LayoutDashboard className="w-5 h-5 text-[#5417d7]" />
+                <h2 className="font-semibold">
+                  Customize your course chapters
+                </h2>
+              </div>
+            </div>
+            <div>
+              <ChapterTitle
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
+              <ChapterDescription
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
+            </div>
+
+            <div>
+              <div className="flex items-center gap-x-2">
+                <Eye className="w-4 h-4  text-[#5417d7]" />
+                <h2 className="font-semibold">Access setting</h2>
+              </div>
+              <AccessForm
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
+            </div>
+          </div>
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center space-x-2">
+                <Video className="w-5 h-5  text-[#5417d7]" />
+                <h2 className="font-semibold">Video for chapter</h2>
+              </div>
+            </div>
+            <div>
+              <ChapterVideoForm
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
             </div>
           </div>
         </div>
       </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10">
-        <div className="space-y-4">
-          <div>
-            <div className="flex items-center gap-x-2 ">
-              <LayoutDashboard className="w-5 h-5 text-[#5417d7]" />
-              <h2 className="font-semibold">Customize your course chapters</h2>
-            </div>
-          </div>
-          <div>
-            <ChapterTitle
-              initialData={chapter}
-              courseId={params.courseId}
-              chapterId={params.chapterId}
-            />
-            <ChapterDescription
-              initialData={chapter}
-              courseId={params.courseId}
-              chapterId={params.chapterId}
-            />
-          </div>
-
-          <div>
-            <div className="flex items-center gap-x-2">
-              <Eye className="w-4 h-4  text-[#5417d7]" />
-              <h2 className="font-semibold">Access setting</h2>
-            </div>
-            <AccessForm
-              initialData={chapter}
-              courseId={params.courseId}
-              chapterId={params.chapterId}
-            />
-          </div>
-        </div>
-        <div className="space-y-4">
-          <div>
-            <div className="flex items-center space-x-2">
-              <Video className="w-5 h-5  text-[#5417d7]" />
-              <h2 className="font-semibold">Video for chapter</h2>
-            </div>
-          </div>
-          <div>
-            <ChapterVideoForm
-              initialData={chapter}
-              courseId={params.courseId}
-              chapterId={params.chapterId}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ components/banner.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { AlertTriangle, CheckCircle } from "lucide-react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const bannerVariants = cva(
+  "border text-center text-sm flex p-4 items-center  w-full",
+  {
+    variants: {
+      variant: {
+        warning: "bg-yellow-200 border-yello-30 text-primary",
+        success: "bg-emarald-700",
+      },
+      defaultVariants: {
+        variant: "warning",
+      },
+    },
+  }
+);
+
+interface BannerProps extends VariantProps<typeof bannerVariants> {
+  label: string;
+}
+
+const iconMap = {
+  warning: AlertTriangle,
+  success: CheckCircle,
+};
+
+const Banner = ({ label, variant }: BannerProps) => {
+  const Icon = iconMap[variant || "warning"];
+  return (
+    <div className={cn(bannerVariants({ variant }))}>
+      <Icon className="w-4 h-4 mr-4" />
+      {label}
+    </div>
+  );
+};
+
+export default Banner;
