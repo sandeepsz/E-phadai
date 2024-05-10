9da9ee8d73cae10ad1c4b6b5f87f142fa3fc7825
sandip <sandeep47nepali@gmail.com>
2024-05-10 15:08:04 +0545

    add search functionality

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { db } from "@/lib/db";
 import Categories from "./_components/categories";
+import SearchInput from "@/components/search-input";
 
 const Searh = async () => {
   const categories = await db.category.findMany({
@@ -11,9 +12,14 @@ const Searh = async () => {
   });
 
   return (
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="py-4 mx-6 md:hidden block ">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   );
 };
 
--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+"use client";
+import { Search } from "lucide-react";
+import qs from "query-string";
+import React, { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import useDebounce from "@/hooks/use-debounce";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 const SearchInput = () => {
-  return <div>SearchInpu</div>;
+  const [value, setValue] = useState("");
+
+  const deboucedValue = useDebounce(value);
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const currentCategoryId = searchParams.get("categoryId");
+
+  useEffect(() => {
+    const handler = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          categoryId: currentCategoryId,
+          title: deboucedValue,
+        },
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(handler);
+  }, [deboucedValue, currentCategoryId, router, pathname]);
+
+  return (
+    <div className="relative">
+      <Input
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+        value={value}
+        className="w-full pl-2 md:w-[400px] bg-slate-200 rounded-sm focus-visible:ring-1 text-md"
+        placeholder="search for a course"
+      />
+
+      <Button className="absolute rounded-l-none bg-[#5325b5] top-0 right-0">
+        <Search className=" w-4 h-4 text-white" />
+      </Button>
+    </div>
+  );
 };
 
 export default SearchInput;
