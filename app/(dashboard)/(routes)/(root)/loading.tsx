269ba0ca8f91013601e2423b69b0c2cf9eec7d6a
sandip <sandeep47nepali@gmail.com>
2024-06-21 17:06:38 +0545

    make responsice shimer UI

--- app/(dashboard)/(routes)/(root)/loading.tsx
+++ app/(dashboard)/(routes)/(root)/loading.tsx
@@ -6,10 +6,10 @@ const Loading = () => {
     <>
       <div className="p-6 space-y-4">
         <div className="grid grid-col-1 sm:grid-cols-2 mt-2 gap-4">
-          <Skeleton className="h-[70px] w-[490px]" />
-          <Skeleton className="h-[70px] w-[490px]" />
+          <Skeleton className=" h-[70px] w-full md:h-[70px] md:w-[490px] " />
+          <Skeleton className=" h-[70px] w-full md:h-[70px] md:w-[490px] " />
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
+        <div className="grid items-center justify-center sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
           <Skeleton className="h-[300px] w-[320px]" />
           <Skeleton className="h-[300px] w-[320px]" />
           <Skeleton className="h-[300px] w-[320px]" />
