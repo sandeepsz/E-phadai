591c21e3d06eb29d637ffda3e9fcfc5b1d4c017c
sandip <sandeep47nepali@gmail.com>
2024-06-21 17:13:40 +0545

    change indicater color

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -12,13 +12,13 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-green-800",
+      "relative h-4 w-full overflow-hidden rounded-full bg-[#9f7ae9]",
       className
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-green-800 transition-all"
+      className="h-full w-full flex-1 bg-[#9371d7] transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
